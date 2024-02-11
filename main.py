fa40a7d9a9262ef1c07f90e1d08d5e090374c1e1
Irene Wahju <97553086+iwahju@users.noreply.github.com>
2024-02-11 01:11:37 -0600

    values updated

--- main.py
+++ main.py
@@ -12,3 +12,23 @@ for page_number in range(1,5):
     url_start = 'https://www.centralcharts.com/en/price-list-ranking/'
     url_end = 'ALL/asc/ts_29-us-nyse-stocks--qc_1-alphabetical-order'
     url = url_start +url_end +str(page_number)
+    pages.append(url)
+
+values_list = []
+
+for page in pages:
+    webpage = requests.get(page)
+    soup = bs(webpage.text, 'html.parser')
+
+    stock_table = soup.find('table', class_='tabMini tabQuotes')
+
+    for each_tr_tag in tr_tag_list[1:]:
+        td_tag_list = each_tr_tag.find_all('td')
+
+        row_values =[]
+        for each_td_tag in td_tag_list[0:7]:
+            new_value = each_td_tag.text.strip()
+            row_values.append(new_value)
+        values_list.append(row_values)
+
+print(values_list)
